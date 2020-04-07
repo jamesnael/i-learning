ce34608ab7a0dfad29ea597aac58cb69c9fa975a
James Nathanael <jamesnael28@gmail.com>
2020-04-07 10:58:47 +0700

    Rapihin codingan

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -34,112 +34,112 @@
     <!-- begin::Body -->
     <body class="m--skin- m-header--fixed m-header--fixed-mobile m-aside-left--enabled m-aside-left--skin-dark m-aside-left--fixed m-aside-left--offcanvas m-footer--push m-aside--offcanvas-default">
         <!-- begin:: Page -->
-            <div class="m-grid m-grid--hor m-grid--root m-page">            
-                <div class="m-grid__item m-grid__item--fluid m-grid m-grid--hor m-login m-login--signin m-login--2 m-login-2--skin-2" id="m_login" style="background-image: url(assets/app/media/img//bg/bg-3.jpg);">
-                    <div class="m-grid__item m-grid__item--fluid m-login__wrapper">
-                        <div class="m-login__container">
-                            <div class="m-login__logo">
-                                <a href="#">
-                                    <img src="{{ asset('images/logo.png') }}" width="40%">     
-                                </a>
+        <div class="m-grid m-grid--hor m-grid--root m-page">            
+            <div class="m-grid__item m-grid__item--fluid m-grid m-grid--hor m-login m-login--signin m-login--2 m-login-2--skin-2" id="m_login" style="background-image: url(assets/app/media/img//bg/bg-3.jpg);">
+                <div class="m-grid__item m-grid__item--fluid m-login__wrapper">
+                    <div class="m-login__container">
+                        <div class="m-login__logo">
+                            <a href="#">
+                                <img src="{{ asset('images/logo.png') }}" width="40%">     
+                            </a>
+                        </div>
+                        <div class="m-login__signin">
+                            <div class="m-login__head">
+                                <h3 class="m-login__title">Sign In To i-Learning System</h3>
                             </div>
-                            <div class="m-login__signin">
-                                <div class="m-login__head">
-                                    <h3 class="m-login__title">Sign In To i-Learning System</h3>
+                            <form class="m-login__form m-form" method="POST" action="">
+                                {{ csrf_field() }}
+                                @error('email')
+                                    <div class="m-alert m-alert--outline alert alert-danger alert-dismissible" role="alert">
+                                        <button type="button" class="close" data-dismiss="alert" aria-label="Close"></button>
+                                        <span>{{ $message }}</span>
+                                    </div>
+                                @enderror
+                                <div class="form-group m-form__group">
+                                    <input class="form-control m-input" type="text" placeholder="Email" name="email" autocomplete="off">
                                 </div>
-                                <form class="m-login__form m-form" method="POST" action="">
-                                    {{ csrf_field() }}
-                                    @error('email')
-                                        <div class="m-alert m-alert--outline alert alert-danger alert-dismissible" role="alert">
-                                            <button type="button" class="close" data-dismiss="alert" aria-label="Close"></button>
-                                            <span>{{ $message }}</span>
-                                        </div>
-                                    @enderror
-                                    <div class="form-group m-form__group">
-                                        <input class="form-control m-input" type="text" placeholder="Email" name="email" autocomplete="off">
-                                    </div>
-                                    <div class="form-group m-form__group">
-                                        <input class="form-control m-input m-login__form-input--last" type="password" placeholder="Password" name="password">
-                                    </div>
-                                    <div class="row m-login__form-sub">
-                                        <div class="col m--align-left m-login__form-left">
-                                            <label class="m-checkbox  m-checkbox--focus">
-                                            <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}> Remember me
-                                            <span></span>
-                                            </label>
-                                        </div>
-                                        <div class="col m--align-right m-login__form-right">
-                                            <a href="javascript:;" id="m_login_forget_password" class="m-link">Forget Password ?</a>
-                                        </div>
-                                    </div>
-                                    <div class="m-login__form-action">
-                                        <button type="submit" class="btn btn-focus m-btn m-btn--pill m-btn--custom m-btn--air m-login__btn m-login__btn--primary" >Sign In</button>
-                                    </div>
-                                </form>
-                            </div>
-                            <div class="m-login__signup">
-                                <div class="m-login__head">
-                                    <h3 class="m-login__title">Sign Up</h3>
-                                    <div class="m-login__desc">Enter your details to create your account:</div>
+                                <div class="form-group m-form__group">
+                                    <input class="form-control m-input m-login__form-input--last" type="password" placeholder="Password" name="password">
                                 </div>
-                                <form class="m-login__form m-form" action="{{ route('register') }}">
-                                    {{ csrf_field() }}
-                                    <div class="form-group m-form__group">
-                                        <input class="form-control m-input" type="text" placeholder="Enter your name" name="fullname" >
+                                <div class="row m-login__form-sub">
+                                    <div class="col m--align-left m-login__form-left">
+                                        <label class="m-checkbox  m-checkbox--focus">
+                                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}> Remember me
+                                        <span></span>
+                                        </label>
                                     </div>
-                                    <div class="form-group m-form__group">
-                                        <input class="form-control m-input" type="text" placeholder="Enter your email" name="email" autocomplete="off">
+                                    <div class="col m--align-right m-login__form-right">
+                                        <a href="javascript:;" id="m_login_forget_password" class="m-link">Forget Password ?</a>
                                     </div>
-                                    <div class="form-group m-form__group">
-                                        <textarea class="form-control m-input" name="address" autocomplete="off" placeholder="Enter your address" rows="3"></textarea>
-                                    </div>
-                                    <div class="form-group m-form__group">
-                                        <input class="form-control m-input" type="password" placeholder="Enter your password" name="password">
-                                    </div>
-                                    <div class="form-group m-form__group">
-                                        <input class="form-control m-input m-login__form-input--last" type="password" placeholder="Enter confirm password" name="rpassword">
-                                    </div>
-                                    <div class="row form-group m-form__group m-login__form-sub">
-                                        <div class="col m--align-left">
-                                            <label class="m-checkbox m-checkbox--focus">
-                                            <input type="checkbox" name="agree">I Agree the <a href="#" class="m-link m-link--focus">terms and conditions</a>.
-                                            <span></span>
-                                            </label>
-                                            <span class="m-form__help"></span>
-                                        </div>
-                                    </div>
-                                    <div class="m-login__form-action">
-                                        <button id="m_login_signup_submit" class="btn btn-focus m-btn m-btn--pill m-btn--custom m-btn--air  m-login__btn" type="submit">Sign Up</button>&nbsp;&nbsp;
-                                        <button id="m_login_signup_cancel" class="btn btn-outline-focus m-btn m-btn--pill m-btn--custom  m-login__btn">Cancel</button>
-                                    </div>
-                                </form>
-                            </div>
-                            <div class="m-login__forget-password">
-                                <div class="m-login__head">
-                                    <h3 class="m-login__title">Forgotten Password ?</h3>
-                                    <div class="m-login__desc">Enter your email to reset your password:</div>
                                 </div>
-                                <form class="m-login__form m-form" action="{{ route('password.email') }}">
-                                    {{ csrf_field() }}
-                                    <div class="form-group m-form__group">
-                                        <input class="form-control m-input" type="text" placeholder="Email" name="email" id="m_email" autocomplete="off">
-                                    </div>
-                                    <div class="m-login__form-action">
-                                        <button id="m_login_forget_password_submit" class="btn btn-focus m-btn m-btn--pill m-btn--custom m-btn--air  m-login__btn m-login__btn--primaryr">Request</button>&nbsp;&nbsp;
-                                        <button id="m_login_forget_password_cancel" class="btn btn-outline-focus m-btn m-btn--pill m-btn--custom m-login__btn">Cancel</button>
-                                    </div>
-                                </form>
+                                <div class="m-login__form-action">
+                                    <button type="submit" class="btn btn-focus m-btn m-btn--pill m-btn--custom m-btn--air m-login__btn m-login__btn--primary" >Sign In</button>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="m-login__signup">
+                            <div class="m-login__head">
+                                <h3 class="m-login__title">Sign Up</h3>
+                                <div class="m-login__desc">Enter your details to create your account:</div>
                             </div>
-                            <div class="m-login__account">
-                                <span class="m-login__account-msg">
-                                Don't have an account yet ?
-                                </span>&nbsp;&nbsp;
-                                <a href="javascript:;" id="m_login_signup" class="m-link m-link--light m-login__account-link">Sign Up</a>
+                            <form class="m-login__form m-form" action="{{ route('register') }}">
+                                {{ csrf_field() }}
+                                <div class="form-group m-form__group">
+                                    <input class="form-control m-input" type="text" placeholder="Enter your name" name="fullname" >
+                                </div>
+                                <div class="form-group m-form__group">
+                                    <input class="form-control m-input" type="text" placeholder="Enter your email" name="email" autocomplete="off">
+                                </div>
+                                <div class="form-group m-form__group">
+                                    <textarea class="form-control m-input" name="address" autocomplete="off" placeholder="Enter your address" rows="3"></textarea>
+                                </div>
+                                <div class="form-group m-form__group">
+                                    <input class="form-control m-input" type="password" placeholder="Enter your password" name="password">
+                                </div>
+                                <div class="form-group m-form__group">
+                                    <input class="form-control m-input m-login__form-input--last" type="password" placeholder="Enter confirm password" name="rpassword">
+                                </div>
+                                <div class="row form-group m-form__group m-login__form-sub">
+                                    <div class="col m--align-left">
+                                        <label class="m-checkbox m-checkbox--focus">
+                                        <input type="checkbox" name="agree">I Agree the <a href="#" class="m-link m-link--focus">terms and conditions</a>.
+                                        <span></span>
+                                        </label>
+                                        <span class="m-form__help"></span>
+                                    </div>
+                                </div>
+                                <div class="m-login__form-action">
+                                    <button id="m_login_signup_submit" class="btn btn-focus m-btn m-btn--pill m-btn--custom m-btn--air  m-login__btn" type="submit">Sign Up</button>&nbsp;&nbsp;
+                                    <button id="m_login_signup_cancel" class="btn btn-outline-focus m-btn m-btn--pill m-btn--custom  m-login__btn">Cancel</button>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="m-login__forget-password">
+                            <div class="m-login__head">
+                                <h3 class="m-login__title">Forgotten Password ?</h3>
+                                <div class="m-login__desc">Enter your email to reset your password:</div>
                             </div>
-                        </div>  
-                    </div>
-                </div>              
-            </div>
+                            <form class="m-login__form m-form" action="{{ route('password.email') }}">
+                                {{ csrf_field() }}
+                                <div class="form-group m-form__group">
+                                    <input class="form-control m-input" type="text" placeholder="Email" name="email" id="m_email" autocomplete="off">
+                                </div>
+                                <div class="m-login__form-action">
+                                    <button id="m_login_forget_password_submit" class="btn btn-focus m-btn m-btn--pill m-btn--custom m-btn--air  m-login__btn m-login__btn--primaryr">Request</button>&nbsp;&nbsp;
+                                    <button id="m_login_forget_password_cancel" class="btn btn-outline-focus m-btn m-btn--pill m-btn--custom m-login__btn">Cancel</button>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="m-login__account">
+                            <span class="m-login__account-msg">
+                            Don't have an account yet ?
+                            </span>&nbsp;&nbsp;
+                            <a href="javascript:;" id="m_login_signup" class="m-link m-link--light m-login__account-link">Sign Up</a>
+                        </div>
+                    </div>  
+                </div>
+            </div>              
+        </div>
         <!-- end:: Page -->
 
         <!--begin::Base Scripts -->
